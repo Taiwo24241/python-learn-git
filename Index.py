6319d7281e9791a89b9788355b2c98bd0c0129d2
Taiwo 24 <taiolanusi@gmail.com>
2025-10-23 17:45:53 +0100

    I added the index.py and assignment.txt. It has only add and sub

---
+++ Index.py
@@ -0,0 +1,34 @@
+print("Welcome to my calculator")
+def calculator():
+    menu = input("1. Add 2. Substraction 3. multiplication")
+    if(menu == "1"):
+        add()
+    elif(menu == "2"):
+        sub()
+    elif(menu == "3"):
+        mul()
+    else:
+        print("Incorrect entry")
+    print("Do you want to do any other thing? ")
+    reload = input("1. yes 2. no ")
+    if reload == "1":
+        calculator()
+    else:
+        print("BYE!")
+
+def add():
+    first = input("Enter first number: ")
+    second = input("Enter second number: ")
+    result = first + second
+    print(result)
+def sub():
+    first = input("Enter first number: ")
+    second = input("Enter second number: ")
+    result = first - second
+    print(result)
+def mul():
+    first = input("Enter first number: ")
+    second = input("Enter second number: ")
+    result = first * second
+    print(result)
+calculator()
