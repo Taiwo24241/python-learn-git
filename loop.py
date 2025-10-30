f218a987efd971c070f7f0c5d4111ab077dd4137
Taiwo 24 <taiolanusi@gmail.com>
2025-10-30 18:38:10 +0100

    Concatination, parameter, and return function

---
+++ loop.py
@@ -0,0 +1,12 @@
+#for and while loop
+# a = 10
+# while a > 0:
+#     print(a)
+#     a = a - 1
+
+start =int(input("Enter starting point: "))
+end = int(input("Enter end point: "))
+while start <= end:
+    print(start)
+    start = start - 1
+    
