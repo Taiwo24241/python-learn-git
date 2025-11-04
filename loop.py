e413bfb9b3743ca7fa051d7fb1163c9ec68b95fb
Taiwo 24 <taiolanusi@gmail.com>
2025-11-04 22:46:05 +0100

    While loop

--- loop.py
+++ loop.py
@@ -1,12 +1,27 @@
 #for and while loop
+# while True:
+#     print("Hello")
+#     print("Hi")
+# print("Good Evening")   
+
 # a = 10
 # while a > 0:
 #     print(a)
 #     a = a - 1
 
+# a = 10
+# while a > 0:
+#     print(a)
+#     a = a - 3
+
 start =int(input("Enter starting point: "))
 end = int(input("Enter end point: "))
 while start <= end:
     print(start)
-    start = start - 1
-    
+    start = start + 1
+
+#Break and continue
+
+
+
+ 
