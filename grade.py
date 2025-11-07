acc172c5cb38ec72d0e3cbebdd0456ffe173173a
Taiwo 24 <taiolanusi@gmail.com>
2025-11-07 16:28:42 +0100

    i made some changes ( changed grade to Score)

--- grade.py
+++ grade.py
@@ -2,15 +2,15 @@
 print("TMO grading system")
 def std_grade(score, total):
        grade = (score / total * 100)
-       if grade >= 70:
+       if score >= 70:
                grade = "A"
-       elif grade >= 60:
+       elif score >= 60:
                grade = "B"
-       elif grade >= 50:
+       elif score >= 50:
                grade = "C"
-       elif grade >= 45:
+       elif score >= 45:
                grade = "D"
-       elif grade >= 40:
+       elif score >= 40:
                grade = "E"
        else: grade = "F" 
        return(f"You scored {score} out of {total} which is grade {grade}")
