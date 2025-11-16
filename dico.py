83728c3f831c250630b357f22cee470c3692f2bb
Taiwo 24 <taiolanusi@gmail.com>
2025-11-16 20:36:02 +0100

    Dictionary have been added

---
+++ dico.py
@@ -0,0 +1,20 @@
+# person = {"name": ["taye", "ken", "idowu"], "school": "UI", "dept": "CSC"}
+# person.clear()
+# print(person)
+# print(person["name"])
+# print(person.keys())
+# print(person.values())
+# person.clear
+# person["age"] = 20  # to add to the dictionary
+# print(person)
+
+# person = {}
+# new_name = input("Enter the name: ")
+# person["name"] = new_name
+# print(person)
+
+# person = {}
+# key = input("Enter the key: ")
+# value = input(f"Enter the value of {key}: ")
+# person[key] = value
+# print(person)
