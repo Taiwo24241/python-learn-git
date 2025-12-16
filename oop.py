89831cfbb18cc598fa362434d6d16ee6e18a53cc
Taiwo 24 <taiolanusi@gmail.com>
2025-12-16 08:15:24 +0100

    i added Constructor and method overriding

--- oop.py
+++ oop.py
@@ -35,3 +35,64 @@ eve.sleep(6, "right")
 
 
 #Constructor
+# The constructor is triggered immediately you create a new instance of a class
+# constructor expects 2 arguments
+# _init_ is the special method while _new_ crwates object instance, _new_ is a more advanced method
+class person:
+    def __init__(self, new_name, new_dept): 
+        self.name = new_name
+        print(self.name, self.dept)
+        self.greet()  
+
+    def say_hi(self):
+        print(f"{self.name} How are you today? ")
+
+    def greet(self):
+        print("Good Evening Everyone")
+
+a = person("Rukayat", "CSC")
+a.say_hi()
+
+
+# CLASS INHERITANCE 
+# Allows new class (child class) to inherit attributes and methods from an existing class (parent class)
+#  parent class or base class and child class or derived class
+class human:
+    eye = 2
+    head = 1
+    ears = 2
+    def talk(self):
+        print("I am talking")
+
+    #To create a method taht displays and eye info
+    def display_head_info(self):
+        print(f" I have {self.eye} in my {self.head} head")
+
+class man(human):
+    # voice = "deep"
+    # To make the voice dynamic
+    # create a constructor first
+    def __init__(self, voice, eye, head, ears):
+        super().__init__(eye=eye, head=head, ears=ears)
+        self.voice = voice
+        #call on the parent constrctor
+        
+        
+
+        super().__init__()
+    def talk(self):  # method overriding 
+        print("I am not talking")  # This talk will overide the talk above, it will print this *talk* for man instead of the one in human
+
+class woman(human):
+    hair = "plenty"
+    def talk(self):
+        super().talk() #super refers to the parent class
+        print("I am not talking") # It will print the parent class " I am talking" first before printing this one
+# adam = man(ears= 4, head= 1, eye= 2)
+print(adam.eye)
+adam.talk
+adam.display_head_info
+
+
+# Method Overriding
+# Where a method overrides another method
